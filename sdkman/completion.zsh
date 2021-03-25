dbd37552e06f2ea2eec27acec7f0b420e09b803e
Jim Hurne <hurne_jim@yahoo.com>
2021-03-25 08:23:50 -0400

    Remove the sdkman dotfiles

--- sdkman/completion.zsh
+++
@@ -1,7 +0,0 @@
-# Setup the shell integration with sdkman
-#
-# This technically not a completion file, but we do want this to be loaded pretty late, and completions.zsh files are loaded last.
-#
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/Users/jhurne/.sdkman"
-[[ -s "/Users/jhurne/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/jhurne/.sdkman/bin/sdkman-init.sh"
--- sdkman/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# SDKMAN!
-#
-
-curl -s "https://get.sdkman.io" | bash
-
-exit 0
-
