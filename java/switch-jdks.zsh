9d99b2a09831986a612f0a9f9779b39eac2eb085
Jim Hurne <hurne_jim@yahoo.com>
2021-03-25 07:28:52 -0400

    Use asdf for managing java instead of homebrew
    asdf is better at managing different versions of software, including
    java.

--- java/switch-jdks.zsh
+++
@@ -1,7 +0,0 @@
-
-# See https://github.com/AdoptOpenJDK/homebrew-openjdk#switch-between-different-jdk-versions
-jdk() {
-        version=$1
-        export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
-        java -version
- }
