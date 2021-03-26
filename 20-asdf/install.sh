2a98d6e314ae742c29fd37e8fb6002962a4d91b7
Jim Hurne <hurne_jim@yahoo.com>
2021-03-26 06:25:58 -0400

    Add install script for asdf and asdf configuration

---
+++ 20-asdf/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Installs asdf packages
+#
+echo
+echo "Installing ASDF plugins"
+
+ASDF_HOME=$(brew --prefix asdf)
+asdf=$ASDF_HOME/bin/asdf
+
+$asdf plugin-add java
+$asdf plugin-add python
+$asdf plugin-add ruby
+$asdf plugin-add gradle
+$asdf plugin-add sbt
+
+exit 0
