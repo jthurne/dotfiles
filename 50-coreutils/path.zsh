0ffe8c2c5a00d15927d9af9cbd13a4ff290c9f6e
Jim Hurne <hurne_jim@yahoo.com>
2021-03-26 08:17:22 -0400

    Fix coreutils path to use brew --prefix
    Things aren't always installed in the places you would expect.

--- 50-coreutils/path.zsh
+++ 50-coreutils/path.zsh
@@ -1,3 +1,3 @@
 # Put the GNU-based core utilities (grep, etc) on the PATH and MAN PATH _before_ the MacOS builtin versions
-PATH="/usr/local/opt/grep/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-MANPATH="/usr/local/opt/grep/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:/usr/local/opt/coreutils/libexec/gnuman:$PATH"
+PATH="$(brew --prefix grep)/gnubin:$(brew --prefix gnu-sed)/libexec/gnubin:$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+MANPATH="$(brew --prefix grep)/gnuman:$(brew --prefix gnu-sed)/libexec/gnuman:$(brew --prefix coreutils)/libexec/gnuman:$PATH"
