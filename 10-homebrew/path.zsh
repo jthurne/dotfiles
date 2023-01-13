9f51758a23ea207b667bdab5f86ffd879f88aa17
Jim Hurne <hurne_jim@yahoo.com>
2023-01-13 07:18:36 -0500

    Setup homebrow properly on Apple silocon machines
    Where homebrew is installed is different depending on the system architecture.
    We can easily check to see where homebrew is installed and call its shellenv
    command from there.

--- 10-homebrew/path.zsh
+++ 10-homebrew/path.zsh
@@ -1,1 +1,6 @@
-eval "$(/usr/local/bin/brew shellenv)"
+if [ -f "/usr/loca/bin/brew" ]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+elif [ -f "/opt/homebrew/bin/brew" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
