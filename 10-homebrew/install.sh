aae23ffd61665605dab99104946d307b6756e81f
Jim Hurne <jhurne@gradle.com>
2023-05-19 13:32:00 -0400

    Evaluate homebrew shell envs after installing

--- 10-homebrew/install.sh
+++ 10-homebrew/install.sh
@@ -12,6 +12,12 @@ if test ! $(which brew)
 then
   echo "Installing Homebrew!"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  if [ -f "/usr/loca/bin/brew" ]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+  elif [ -f "/opt/homebrew/bin/brew" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 fi
 
 # Run Homebrew through the Brewfile
@@ -19,4 +25,3 @@ echo "Updating/installing packages with Homebrew"
 brew bundle --file="$scriptdir/Brewfile"
 
 exit 0
-
