d46136dada55a3b5f26ccb6d370f522bae2002dc
Jim Hurne <jhurne@gradle.com>
2023-05-19 11:55:52 -0400

    Moderninze the Homebrew installation script

--- 10-homebrew/install.sh
+++ 10-homebrew/install.sh
@@ -11,16 +11,7 @@ echo ""
 if test ! $(which brew)
 then
   echo "Installing Homebrew!"
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Run Homebrew through the Brewfile
