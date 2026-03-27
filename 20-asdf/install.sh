828231cbdb90a4d52abde5a3b6de70309f0dc8b8
Jim Hurne <jhurne@gradle.com>
2026-03-27 10:40:59 -0400

    Simplify asdf install script
    Remove ASDF_HOME indirection since asdf is on PATH via Homebrew.
    Remove exit 0 that was masking failures from the old plugin-add syntax.

--- 20-asdf/install.sh
+++ 20-asdf/install.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 #
-# Installs asdf packages
+# Installs asdf plugins
 #
+
 echo
 echo "Installing ASDF plugins"
 
-ASDF_HOME=$(brew --prefix asdf)
-asdf=$ASDF_HOME/bin/asdf
-
-$asdf plugin add java
-$asdf plugin add python
-$asdf plugin add ruby
-$asdf plugin add gradle
-$asdf plugin add sbt
-$asdf plugin add nodejs
-
-exit 0
+asdf plugin add java
+asdf plugin add python
+asdf plugin add ruby
+asdf plugin add gradle
+asdf plugin add sbt
+asdf plugin add nodejs
